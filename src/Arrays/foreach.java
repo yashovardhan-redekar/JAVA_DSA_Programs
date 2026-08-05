5b5645abb123d54cff513a0ec06983ec09fd9aaa
yashovardhan-redekar <redekaryashovardhan10@gmail.com>
2026-08-05 10:15:51 +0530

    arrays

---
+++ src/Arrays/foreach.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class foreach {
+    public static void main(String[] args) {
+        int[] arr ={10,20, 40, 30, 50};
+
+        for( int value : arr){
+            System.out.println(value);
+        }
+
+
+    }
+}
---
+++ src/Arrays/initilization.java
@@ -0,0 +1,16 @@
+package Arrays;
+
+public class initilization {
+    public static void main(String[] args) {
+        int arr[] = new int[5];
+        int arr1[] = {10, 20, 30, 40, 50};
+
+        for(int i =0; i < arr1.length; i++){
+            System.out.println(arr1[i]);
+        }
+
+//        System.out.println("element at index 0 : " +arr1[0]);
+//        System.out.println("element at index 1 : " +arr1[1]);
+//        System.out.println("element at index 2 : " +arr1[2]);
+    }
+}
